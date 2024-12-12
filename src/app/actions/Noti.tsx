ccc2a8c9347e14ebc9d423da72249be0ef4be515
tienpmath <designdalat@gmail.com>
2024-12-12 22:21:46 +0700

    init2

--- src/app/actions/Noti.tsx
+++ src/app/actions/Noti.tsx
@@ -1,19 +1,12 @@
+"use client";
 
-'use client'
-
-
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-
+//import { ToastContainer, toast } from 'react-toastify';
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Noti() {
-    // const header = props;
-    toast("Bạn đã gửi dy");
-    //  console.log(header);
-    console.log("AAAAA");
-    return (
-        <>
-            <ToastContainer />
-        </>
-    )
+  // const header = props;
+  //toast("Bạn đã gửi dy");
+  //  console.log(header);
+  console.log("AAAAA");
+  return <>{/* <ToastContainer /> */}</>;
 }
